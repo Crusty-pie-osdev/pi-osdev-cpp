0cf3e50a8aa261fd147e06b7505ab90cc8f753eb
Pierre Masselot <pierre.masselot.d@gmail.com>
2024-09-15 18:34:01 -0400

    use a constants name space

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,15 +1,15 @@
-#include "./io/io.hpp"
+#include "Constants.h"
+#include "io/io.h"
 
-int main()
-{
-    const char *helloMessage = "Hello, World!\n";
-    uart_init();
-    uart_writeText(helloMessage);
+int main() {
+  const char *helloMessage = "Hello, World!\n";
+  uart_init();
+  uart_writeText(helloMessage);
+  uart_writeText("This is a test message.\n");
 
-    while (1)
-    {
-        ;
-    }
+  while (1) {
+    ;
+  }
 
-    return 0;
+  return 0;
 }
